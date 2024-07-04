a8dc014054c699fc9bf832912f95dc1c52f3a08a
JuliaHown <juliahown87@gmail.com>
2024-07-04 16:15:43 -0300

    estudo sobre arrays

---
+++ Introdução/fundamentos/array.ts
@@ -0,0 +1,11 @@
+const array: Array<Number> = [1, 2, 3, 4]
+
+const stringArray: string[] = ['a', 'b', 'c']
+
+// console.log(array[1])
+
+console.log('Array original ', array)
+
+array.push(5)
+
+console.log('Array com novo elemento ', array)
